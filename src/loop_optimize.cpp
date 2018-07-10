355c2ff74e487b9c5023da5aec0d9bbb343e98ea
Arjan Gupta <arjan.gupta@eyeverify.com>
2018-07-09 22:57:31 -0500

    Chunk the data into fours, not the whole size at a time. Loop will still be used

--- src/loop_optimize.cpp
+++ src/loop_optimize.cpp
@@ -5,7 +5,7 @@
 namespace simd_optimizations
 {
 
-    float calculateNormalLoop(const float* arr1, const float* arr2, const std::size_t size)
+    float calculateNormalLoop(const float* arr1, const float* arr2, std::size_t size)
     {
         float result = 0;
 
@@ -18,19 +18,20 @@ namespace simd_optimizations
         return result;
     }
 
-    float calculateOptimizedLoop(const float* arr1, const float* arr2, const std::size_t size)
+    float calculateOptimizedLoop(const float* arr1, const float* arr2, std::size_t size)
     {
-        //Convert arrays to simd vectors
-        simdpp::float32<size> arr1_simdvec = simdpp::load(arr1);
-        simdpp::float32<size> arr2_simdvec = simdpp::load(arr2);
-        
-        //Algo
-        simdpp::float32<size> diff    = simdpp::sub(arr1_simdvec, arr2_simdvec);
-        simdpp::float32<size> squares = simdpp::mul(diff, diff);
-        simdpp::float32<size> result  = simdpp::reduce_add(squares);
+        float result = 0;
 
-        float hello = 0.0;
-        return hello;
+        for (size_t i = 0; i < size; i += 4)
+        {
+            simdpp::float32<4> arr1_simdvec = simdpp::load(arr1 + i);
+            simdpp::float32<4> arr2_simdvec = simdpp::load(arr2 + i);
+            simdpp::float32<4> diff = simdpp::sub(arr1_simdvec, arr2_simdvec);
+            simdpp::float32<4> squares = simdpp::mul(diff, diff);
+            result += simdpp::reduce_add(squares);
+        }
+
+        return result;
     }
 
 } //end namespace simd_optimizations
