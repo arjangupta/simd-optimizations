d97825a0aebd8f06655c7dad1bb971d3331844f1
Arjan Gupta <arjan.gupta@eyeverify.com>
2018-07-09 23:20:45 -0500

    Remove namespace indentation for src funcs

--- src/loop_optimize.cpp
+++ src/loop_optimize.cpp
@@ -5,34 +5,34 @@
 namespace simd_optimizations
 {
 
-    float calculateNormalLoop(const float* arr1, const float* arr2, std::size_t size)
+float calculateNormalLoop(const float* arr1, const float* arr2, std::size_t size)
+{
+    float result = 0;
+
+    for (size_t i = 0; i < size; ++i)
     {
-        float result = 0;
-
-        for (size_t i = 0; i < size; ++i)
-        {
-            float diff = arr1[i] - arr2[i];
-            result += diff * diff;
-        }
-
-        return result;
+        float diff = arr1[i] - arr2[i];
+        result += diff * diff;
     }
 
-    float calculateOptimizedLoop(const float* arr1, const float* arr2, std::size_t size)
+    return result;
+}
+
+float calculateOptimizedLoop(const float* arr1, const float* arr2, std::size_t size)
+{
+    float result = 0;
+    const int data_points = 8;
+
+    for (size_t i = 0; i < size; i += data_points)
     {
-        float result = 0;
-        const int data_points = 8;
-
-        for (size_t i = 0; i < size; i += data_points)
-        {
-            simdpp::float32<data_points> arr1_simdvec = simdpp::load(arr1 + i);
-            simdpp::float32<data_points> arr2_simdvec = simdpp::load(arr2 + i);
-            simdpp::float32<data_points> diff = simdpp::sub(arr1_simdvec, arr2_simdvec);
-            simdpp::float32<data_points> squares = simdpp::mul(diff, diff);
-            result += simdpp::reduce_add(squares);
-        }
-
-        return result;
+        simdpp::float32<data_points> arr1_simdvec = simdpp::load(arr1 + i);
+        simdpp::float32<data_points> arr2_simdvec = simdpp::load(arr2 + i);
+        simdpp::float32<data_points> diff = simdpp::sub(arr1_simdvec, arr2_simdvec);
+        simdpp::float32<data_points> squares = simdpp::mul(diff, diff);
+        result += simdpp::reduce_add(squares);
     }
+
+    return result;
+}
 
 } //end namespace simd_optimizations
